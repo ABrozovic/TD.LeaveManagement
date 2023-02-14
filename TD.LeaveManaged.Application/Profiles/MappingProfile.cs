adb55604291c4ea5a8044ff0ac6dbc5baf72be67
Brozovic <alejandro.brozovic@gmail.com>
2023-02-13 21:23:39 -0400

    feat: add automapper and a basic profile configuration

---
+++ TD.LeaveManaged.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using TD.LeaveManaged.Application.DTOs;
+using TD.LeaveManaged.Application.DTOs.LeaveRequest;
+using TD.LeaveManagement.Domain;
+
+namespace TD.LeaveManaged.Application.Profiles
+{
+    public class MappingProfile : Profile 
+    {
+        public MappingProfile()
+        {
+            CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+        }
+    }
+}
